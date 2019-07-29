d7aad7762ae35a356e9d84150f7a806ccb56c089
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-07-29 10:47:19 +0200

    Task4 complete

--- DictionaryCollections/DictionaryCollections/Program.cs
+++ DictionaryCollections/DictionaryCollections/Program.cs
@@ -31,7 +31,6 @@ namespace DictionaryCollections
 	            {
                     Console.WriteLine(lookUp[digit]);
                 }
-	
             }
             Console.ReadLine();
         }
---
+++ LookupCollections/LookupCollections/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookupCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListDictionary list = new ListDictionary();
+            list.Add("Estados Unidos", "United States");
+            list.Add("Canadá", "Canada");
+            list.Add("España", "Spain");
+            Console.WriteLine(list["España"]);
+            Console.WriteLine(list["Canadá"]);
+            Console.ReadLine();
+        }
+    }
+}
