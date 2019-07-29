b0a74211745dcf4d01eb44863b3942ef368e505c
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-07-29 09:25:10 +0200

    Task1 complete

---
+++ SequentialCollections/SequentialCollections/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequentialCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue("First");
+            queue.Enqueue("Second");
+            queue.Enqueue("Third");
+            queue.Enqueue("Forth");
+
+            while (queue.Count > 0)
+            {
+                object obj = queue.Dequeue();
+
+                Console.WriteLine("From Queue: {0}", obj);
+            }
+            Console.Read();
+        }
+    }
+}
