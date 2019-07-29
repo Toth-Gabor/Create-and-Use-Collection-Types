6369833ed7324ec6118ff49a0e14005b304a4732
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-07-29 10:38:39 +0200

    Task3 complete

---
+++ DictionaryCollections/DictionaryCollections/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable lookUp = new Hashtable();
+            lookUp.Add("0", "Zero");
+            lookUp.Add("1", "One");
+            lookUp.Add("2", "Two");
+            lookUp.Add("3", "Three");
+            lookUp.Add("4", "Four");
+            lookUp.Add("5", "Five");
+            lookUp.Add("6", "Six");
+            lookUp.Add("7", "Seven");
+            lookUp.Add("8", "Eight");
+            lookUp.Add("9", "Nine");
+
+            String myNumber = "124556632";
+            foreach (char c in myNumber)
+            {
+                string digit = c.ToString();
+                if (lookUp.ContainsKey(digit))
+	            {
+                    Console.WriteLine(lookUp[digit]);
+                }
+	
+            }
+            Console.ReadLine();
+        }
+    }
+}
