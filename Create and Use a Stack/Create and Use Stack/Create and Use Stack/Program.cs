af8502dbea7c8dbd3c7a3a31fbe42c0dcb75d613
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-07-29 09:44:11 +0200

    Task2 complete

---
+++ Create and Use a Stack/Create and Use Stack/Create and Use Stack/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create_and_Use_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue("First");
+            queue.Enqueue("Second");
+            queue.Enqueue("Third");
+            queue.Enqueue("Forth");
+
+            Stack stack = new Stack();
+            stack.Push("First");
+            stack.Push("Second");
+            stack.Push("Third");
+            stack.Push("Forth");
+
+            while (queue.Count > 0)
+            {
+                object obj = queue.Dequeue();
+
+                Console.WriteLine("From Queue: {0}", obj);
+            }
+            Console.WriteLine("############");
+
+            while (stack.Count > 0)
+            {
+                object obj1 = stack.Pop();
+
+                Console.WriteLine("From Stack: {0}", obj1);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
